884634e13ca2b214ab9c999e1fa49e96e4cd7d61
Jung-Ming <m3x0613756a@gmail.com>
2025-01-24 21:47:06 +0800

    V2.0

---
+++ Output/ex03_日期區間.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+
+def 取得日期區間(起始日期, 結束日期):
+    起始日期 = datetime.strptime(起始日期, "%Y/%m/%d")
+    結束日期 = datetime.strptime(結束日期, "%Y/%m/%d")
+
+    日期區間 = []
+    當前日期 = 起始日期
+
+    while 當前日期 <= 結束日期:
+        日期區間.append(當前日期.strftime('%#m/%#d'))
+        當前日期 += timedelta(days=1)
+
+    return 日期區間
+
+
+if __name__ == '__main__':
+    起始日期 = '2023/06/24'
+    結束日期 = '2023/06/30'
+    取得日期區間(起始日期, 結束日期)
